6c358e3f04ce48fa0ae4420df280d93c1d5cfcdc
Ignacio C <icorrao90@gmail.com>
2024-04-05 21:51:25 -0300

    commit 1

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
