78a10700cd5104557e24cf7c7929f8d0cb686cb5
Ignacio C <icorrao90@gmail.com>
2024-04-09 13:19:37 -0300

    commit 5

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("index");
