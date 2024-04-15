cef556643ac7e458ec94acdc8843081d3fc68cda
Ignacio C <icorrao90@gmail.com>
2024-04-15 15:36:05 -0300

    responsive cambios

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.get("/", (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/bootstrap", express.static(path.join(__dirname, "node_modules/bootstrap/dist")));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
